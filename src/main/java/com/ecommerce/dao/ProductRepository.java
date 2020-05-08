82d6d356e22b1c8a2dbaf9e03f216748ccaa9f31
dueduardoful <dueduardoful@gmail.com>
2020-05-08 11:26:16 -0300

    release 1.0

--- src/main/java/com/ecommerce/dao/ProductRepository.java
+++ src/main/java/com/ecommerce/dao/ProductRepository.java
@@ -2,6 +2,7 @@ package com.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.ecommerce.model.Product;
 
@@ -9,7 +10,7 @@ import com.ecommerce.model.Product;
 // collectionResouceRel é o nome da entrada JSON
 // path é o caminho de categoria produtos
 
-
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "products", path = "products")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
